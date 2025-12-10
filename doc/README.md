9ac5b27946afe44b1306307e9a1fd20c92494c93
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2025-12-10 05:15:54 +0000

    document all the addition intrinsics

--- doc/README.md
+++ doc/README.md
@@ -5,29 +5,45 @@ This somewhat convoluted contraption is my attempt to make a more condensed docu
 Intel has a decent page called the [Intrinsics Guide](https://www.intel.com/content/www/us/en/docs/intrinsics-guide/index.html),
 but it literally lists every single instruction (over 7000) without attempting to group their descriptions.
 For example, `_mm256_abs_epi16`, `_mm256_abs_epi8`, and `_mm512_abs_epi8` do essentially the same thing but on differently
-sized integers. But they're still listed as separate instructions in the list.
+sized integers. But they're still listed as separate entries.
+
+I'm not planning to provide the most accurate or complete documentation. If you want to use a function in your program,
+you should definitely read the Intel Instrinsics Guide, which this thing links to for every function.
+This is more of a quick location and discovery tool. (did you know 52-integers are a thing? now I do, but still
+not entirely sure why)
 
 I don't have AVX-512, so I'm only doing this for AVX-2 and earlier.
+
+---
 
 The basic principle is that there's a `./doc.in` file with directives like these:
 ```
 @techs MMX,SSE_ALL,AVX_ALL
 
+# This is a comment
+
 @pattern _mm[256]_add_p{s,d}
 Add vectors of f32/f64 elementwise.
 
-@exhaust .*(?!<lo)add.*
+@pattern +dpx _mm_add_si64
+Silly instruction to add two `u64`/`i64`s. Just use `+`.
+
+@exhaust _mm_.*(?<!lo)add.*
+@exhaust-todo .*add.*
 ```
 
 This file can be validated and converted to HTML with `doc.htmlbuild`. These directives are supported:
 
 - `@techs` selects which "techs" (`MMX,SSE_ALL,AVX_ALL,AVX_512,AMX,SVML,Other`) to load
 - `@pattern` starts a new paragraph describing the selected intrinsics.
-    In this example, it's going to select `_mm_add_ps`, `_mm_add_pd`, `_mm256_add_ps`, `_mm256_add_pd`
+    - In the first example, it's going to select `_mm_add_ps`, `_mm_add_pd`, `_mm256_add_ps`, `_mm256_add_pd`
+    - In the second example, it's going to select only `_mm_add_si64` and also mark it as "deprecated". It's not actually
+    deprecated, but you probably have better ways to add two 64-bit numbers; it's also not in `core::arch::x86` in Rust.
 - `@exhaust` with a regular expression ([Python3 flavor](https://docs.python.org/3/library/re.html#regular-expression-syntax))
     asserts that the patterns so far cover every intrinsic in the selected techs matching the pattern.
     In this case we want to ensure that we cover all the instructions concerned with addition (and
     excluding `_mm_loaddup_pd` which happens to contain the substring "add").
+- `@exhaust-todo` is like `@exhaust`, but produces a warning instead of an error
 
 ## Instructions:
 
@@ -45,4 +61,4 @@ as a JS string in the `data.js` file (WTF, Intel?). You'll need to extract it yo
 `python3 -m doc.htmlbuild > out.html`
 
 You can optionally set the `INTEL_BASE_URL` environment variable if you want to use a different root
-for links to the Intel Intrinsics Guide.
+for links to the Intel Intrinsics Guide (e.g. use the offline version).
--- doc/htmlbuild.py
+++ doc/htmlbuild.py
@@ -1,8 +1,8 @@
-from collections.abc import Iterable
 import os
 import re
 import sys
 from collections import defaultdict
+from collections.abc import Iterable
 from dataclasses import dataclass
 from pathlib import Path
 from typing import assert_never
@@ -38,10 +38,12 @@ class InsPattern:
     title: str
     pat: str
     lines: list[str]
+    deprecated: bool
 
 @dataclass
 class InsExhaust:
     pat: re.Pattern[str]
+    hard: bool
 
 
 type Instruction = InsTechs | InsPattern | InsExhaust
@@ -55,15 +57,6 @@ class ParseError(Exception):
     def __str__(self) -> str:
         return f"at line {self.lineno}: {self.message}"
 
-@dataclass
-class VerifyError(Exception):
-    messages: list[tuple[int, str]]
-
-    def __str__(self) -> str:
-        return "\n".join([
-            f"at line {lineno}: {message}"
-            for lineno, message in self.messages])
-
 
 def parse_instructions(lines: Iterable[str]) -> list[tuple[int, Instruction]]:
     instructions: list[tuple[int, Instruction]] = []
@@ -73,10 +66,7 @@ def parse_instructions(lines: Iterable[str]) -> list[tuple[int, Instruction]]:
 
     for lineno, line in enumerate(lines, start=1):
         line = line.strip()
-        if not line:
-            continue
-
-        if line.startswith("@techs"):
+        if line.startswith("@techs "):
             if techs_seen:
                 raise ParseError(lineno, "duplicate @techs directive")
             techs_seen = True
@@ -87,22 +77,40 @@ def parse_instructions(lines: Iterable[str]) -> list[tuple[int, Instruction]]:
             if not techs_seen:
                 raise ParseError(lineno, "expected @techs directive first")
 
-            if line.startswith("@pattern"):
+            if line.startswith("@pattern "):
                 title = line.split(" ", 1)[1].strip()
+
+                if title.startswith("+dpx"):
+                    title = title.split(" ", 1)[1]
+                    deprecated = True
+                else:
+                    deprecated = False
+
                 pattern = title.split("(",1)[0].strip()
-                ins = InsPattern(title, pattern, [])
+                ins = InsPattern(title, pattern, [], deprecated=deprecated)
                 instructions.append((lineno, ins))
                 current_pattern = ins
-            elif line.startswith("@exhaust"):
+            elif line.startswith("@exhaust "):
                 regex = re.compile(line.split(" ", 1)[1].strip())
-                instructions.append((lineno, InsExhaust(regex)))
+                instructions.append((lineno, InsExhaust(regex, hard=True)))
+            elif line.startswith("@exhaust-todo "):
+                regex = re.compile(line.split(" ", 1)[1].strip())
+                instructions.append((lineno, InsExhaust(regex, hard=False)))
             else:
                 raise ParseError(lineno, "unknown directive")
         else:
-            if not line.startswith("#"):
+            if not line.startswith("#") :
                 if current_pattern is None:
-                    raise ParseError(lineno, "unexpected plain line")
-                current_pattern.lines.append(line)
+                    if line:
+                        raise ParseError(lineno, "unexpected plain line")
+                else:
+                    current_pattern.lines.append(line)
+
+    for lineno, instruction in instructions:
+        # remove trailing blank lines
+        if isinstance(instruction, InsPattern):
+            while instruction.lines[-1:] == [""]:
+                instruction.lines.pop()
 
     return instructions
 
@@ -114,13 +122,23 @@ class Paragraph:
     title: str
     expansions: list[str]
     lines: list[str]
+    deprecated: bool
+
+
+@dataclass
+class Doc:
+    techs: list[str]
+    warnings: list[tuple[int, str]]
+    errors: list[tuple[int, str]]
+    paragraphs: list[Paragraph]
 
 
 # returns (techs, paragraphs)
-def produce_doc(instructions: Iterable[tuple[int, Instruction]]) -> tuple[list[str], list[Paragraph]]:
+def produce_doc(instructions: Iterable[tuple[int, Instruction]]) -> Doc:
     paragraphs: list[Paragraph] = []
 
     errors: list[tuple[int, str]] = []
+    warnings: list[tuple[int, str]] = []
 
     selected_techs: list[str] = []
     selected_intrinsics: set[str] = {*()}
@@ -130,34 +148,43 @@ def produce_doc(instructions: Iterable[tuple[int, Instruction]]) -> tuple[list[s
         match instruction:
             case InsTechs(techs):
                 if unknown_techs := set(techs) - tech_to_intrinsics.keys():
-                    raise VerifyError([(lineno, f"unknown techs: {unknown_techs}")])
+                    errors.append((lineno, f"unknown techs: {unknown_techs}"))
+                    break
                 selected_techs = techs
                 selected_intrinsics = set(i for tech in techs for i in tech_to_intrinsics[tech])
 
-            case InsExhaust(regex):
+            case InsExhaust(regex, hard):
+
                 if not any(regex.fullmatch(i) for i in selected_intrinsics):
                     errors.append((lineno, f"regex {regex} doesn't match any intrinsics"))
 
                 unhandled = [i for i in selected_intrinsics - handled_intrinsics if regex.fullmatch(i)]
-                patterns = globify(unhandled)
-                for pat in patterns:
-                    errors.append((lineno, f"{pat} not handled"))
+                error_dest = errors if hard else warnings
+                for pat in globify(unhandled):
+                    error_dest.append((lineno, f"{pat} not handled"))
 
-            case InsPattern(title, pattern, lines):
+            case InsPattern(title, pattern, lines, deprecated):
                 intrinsics = unglob(pattern)
                 expansions = selected_intrinsics.intersection(intrinsics)
                 if not expansions:
                     errors.append((lineno, f"pattern {pattern} doesn't match anything in {selected_techs}"))
                 handled_intrinsics.update(intrinsics)
-                paragraphs.append(Paragraph(title, sorted(expansions), lines))
+                paragraphs.append(Paragraph(
+                    title,
+                    sorted(expansions),
+                    lines,
+                    deprecated=deprecated,
+                ))
 
             case other:
                 assert_never(other)
 
-    if errors:
-        raise VerifyError(errors)
-    else:
-        return selected_techs, paragraphs
+    return Doc(
+        techs=selected_techs,
+        warnings=warnings,
+        errors=errors,
+        paragraphs=paragraphs,
+    )
 
 
 try:
@@ -166,19 +193,31 @@ except ParseError as e:
     sys.stderr.write(f"htmlbuild.py: parse error:\n{e}\n")
     sys.exit(1)
 
-try:
-    techs, paragraphs = produce_doc(instructions)
-except VerifyError as e:
-    sys.stderr.write(f"htmlbuild.py: did not verify doc:\n{e}\n")
+doc = produce_doc(instructions)
+
+if doc.errors:
+    sys.stderr.write("htmlbuild.py: doc produced errors:\n")
+    maxlen = max(len(str(lineno)) for lineno, _ in doc.errors)
+    for lineno, warning in doc.errors:
+        sys.stderr.write(f"  L{lineno:<{maxlen}}: {warning}\n")
+
+if doc.warnings:
+    sys.stderr.write("htmlbuild.py: doc produced warnings: \n")
+    maxlen = max(len(str(lineno)) for lineno, _ in doc.warnings)
+    for lineno, warning in doc.warnings:
+        sys.stderr.write(f"  L{lineno:<{maxlen}}: {warning}\n")
+
+if doc.errors:
+    sys.stderr.write("htmlbuild.py: not producing a document\n")
     sys.exit(1)
 
-base_url_with_techs = base_url + "#techs=" + ",".join(techs)
-
+base_url_with_techs = base_url + "#techs=" + ",".join(doc.techs)
 
 def backticks_to_html(line: str) -> str:
     return re.sub(r"`([^`]+)`", r"<code>\1</code>", line)
 
 
+# Welcome to 1999
 print("<!doctype html><html>")
 print("<!-- auto-generated by `python -m doc.htmlbuild`, do not edit directly -->")
 print("""
@@ -187,18 +226,27 @@ print("""
     margin: 0;
     padding: 0;
     font-family: system-ui, sans-serif;
+    text-rendering: optimizeSpeed;
 }
 
 hr { margin-bottom: 1rem;  }
 html { padding: 0.5rem 1rem; }
-h2 { font-size: 1.2rem; }
+h2 { font-size: 1.2rem; margin-top: 1.5rem; }
 li { margin-left: 1.5rem; }
-summary { font-size: 1rem; color: #315FA0; cursor: pointer; }
-.pattern { margin-bottom: 1rem; }
+summary {
+    font-size: 1rem;
+    color: #315FA0;
+    cursor: pointer;
+}
 .pattern-description { max-width: 50em; }
 
+h2.deprecated {
+    color: #807070;
+    font-style: oblique 15deg;
+}
+
 .tt, code {
-    font-family: ui-monospace, 'Cascadia Code', 'Source Code Pro', Menlo, Consolas, 'DejaVu Sans Mono', monospace;
+    font-family: 'JuliaMono', ui-monospace, 'Cascadia Code', 'Source Code Pro', Menlo, Consolas, 'DejaVu Sans Mono', monospace;
 }
 
 code {
@@ -208,16 +256,17 @@ code {
 </style></head>
 """)
 print("<body>")
-print("<p>Techs covered: " + ",".join(techs) + "</p>")
+print("<p>Techs covered: " + ",".join(doc.techs) + "</p>")
 print("<hr>")
 
-for para in paragraphs:
-    print(f"<h2 class='tt'>{para.title}</h2>")
+for para in doc.paragraphs:
+    deprecated = " deprecated" if para.deprecated else ""
+    print(f"<h2 class='tt{deprecated}'>{para.title}</h2>")
     print("<div class=pattern>")
 
     if len(para.expansions) > 1:
         print("<details>")
-        print("<summary>Covered instructions</summary>")
+        print(f"<summary>Expansions ({len(para.expansions)})</summary>")
         print("<ul>")
         for intr in para.expansions:
             print(f'<li><a class="tt" href="{base_url_with_techs}&text={intr}">{intr}</a></li>')
@@ -227,9 +276,13 @@ for para in paragraphs:
         [exp] = para.expansions
         print(f'<p><a class="tt" href="{base_url_with_techs}&text={exp}">{exp}</a></p>')
 
-    print("<p class=pattern-description>")
+    print("<div class='pattern-description'>")
+    print("<p>")
     for line in para.lines:
-        print(backticks_to_html(line))
-    print("</p>")
+        if not line.strip():
+            print("<p>")
+        else:
+            print(backticks_to_html(line))
+    print("</div>")
     print("</div>")
 print("</body></html>")
